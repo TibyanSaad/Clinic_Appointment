a824a4960b8c7224a594a779d1c913059558edcd
TibyanSaad <saad.tibyan02@gmail.com>
2026-06-22 09:49:28 +0400

    initial springboot structure

---
+++ Clinic_Appointment/src/main/java/com/clinic/Clinic_Appointment/ClinicAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.Clinic_Appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAppointmentApplication.class, args);
+	}
+
+}
